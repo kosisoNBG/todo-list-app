47c34494998b30517ae41931c6b58589e18820ac
kosisoNBG <devinefavour@gmail.com>
2024-06-02 09:56:16 +0100

    added comments

--- src/Todo.js
+++ src/Todo.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from 'react' // Importing React 
 
 const Todo = ({todo, index, completeTodo, removeTodo}) => {
   return (
+    // Main structure of a single to-do item
     <div style={{textDecoration: todo.isCompleted ? "line-through" : ""}} className='todo'>
-    {todo.text}
+    {todo.text}  {/* Displaying the to-do text */}
     <div>
+      {/* Button to mark the to-do as complete */}
         <button onClick={() => completeTodo(index)}>Complete</button>
+        {/* Button to remove the to-do item */}
         <button onClick={() => removeTodo(index)}>Delete</button>
     </div>
     </div>
   )
 }
 
-export default Todo
+export default Todo // Exporting the Todo component as the default export
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import Todo from './Todo'
+import React from 'react' // Importing React
+import Todo from './Todo' // Importing the Todo component
 
 const TodoList = ({todos, completeTodo, removeTodo}) => {
   return (
     <div>
+      {/* looping through the todos and rendering a Todo component for each item */}
         {todos.map((todo, index) => (
             <Todo 
-            key={index}
-            index={index}
-            todo={todo}
-            completeTodo={completeTodo}
-            removeTodo={removeTodo}
+            key={index} // Unique key for each item 
+            index={index} // Index of the current item 
+            todo={todo}  // The current to-do item 
+            completeTodo={completeTodo} // Function to toggle completion
+            removeTodo={removeTodo} // Function to remove the item 
             />
         ))}
     </div>
   )
 }
 
-export default TodoList
+export default TodoList  //Exporting the TodoList component as the default export
